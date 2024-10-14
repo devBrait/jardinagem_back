0e014c087038440eb382782428cc12aa4d64d221
brait <lorimiervb@gmail.com>
2024-10-14 13:17:34 -0300

    rota do swagger para o render

--- src/app/swagger.ts
+++ src/app/swagger.ts
@@ -1,22 +1,22 @@
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
 
 const swaggerSpec = swaggerJsdoc({
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'API jardinagem',
-            version: '1.0.0',
-            description: 'API do projeto jardinagem usando nodeJS (express)',
-        },
-        servers: [
-            {
-                url: 'http://localhost:8080/v1',
-                description: 'API de teste',
-            },
-        ],
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API jardinagem',
+      version: '1.0.0',
+      description: 'API do projeto jardinagem usando nodeJS (express)',
     },
-    apis: ['./src/routes/*.ts'],
-});
+    servers: [
+      {
+        url: 'https://jardinagem-back.onrender.com/v1',
+        description: 'API de teste',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.ts'],
+})
 
-export { swaggerSpec, swaggerUi };
+export { swaggerSpec, swaggerUi }
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,16 @@ import express from 'express'
 import { swaggerSpec, swaggerUi } from './app/swagger'
 import mainRouter from './routes/route'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 const app = express()
 const port = process.env.PORT || 8080
+
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config({ path: '.env.development' })
+} else {
+  dotenv.config({ path: '.env.production' })
+}
 
 app.use(cors())
 app.use(express.json())
