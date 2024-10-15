f36ca307479c9788706c5c6dd09c584c171039fd
brait <lorimiervb@gmail.com>
2024-10-15 19:02:09 -0300

    acerto de imports errados

--- src/api/repositories/pedidoRepository.ts
+++ src/api/repositories/pedidoRepository.ts
@@ -1,14 +1,12 @@
-import { prisma } from "database/prisma";
-
+import { prisma } from '../../database/prisma'
 
 export const retornaPedido = async (id: number) => {
-    try{
-        const pedido = prisma.pedido.findUnique({
-            where: {id : id}
-        })
-        return pedido
-    }
-    catch(error){
-        throw new Error(`Erro: ID não existe ${error.message}}`)
-    }
+  try {
+    const pedido = prisma.pedido.findUnique({
+      where: { id: id },
+    })
+    return pedido
+  } catch (error) {
+    throw new Error(`Erro: ID não existe ${error.message}}`)
+  }
 }
--- src/api/services/pedidoService.ts
+++ src/api/services/pedidoService.ts
@@ -1,30 +1,29 @@
 import { prisma } from '../../database/prisma'
-import { retornaPedido } from 'api/repositories/pedidoRepository'
+import { retornaPedido } from '../repositories/pedidoRepository'
 
 export const createAsync = async data => {
-    const { cliente, data_criacao, status, valor_total, pedidoItems } = data
+  const { cliente, data_criacao, status, valor_total, pedidoItems } = data
 
-    const data_criacao_formatada = new Date(data_criacao)
+  const data_criacao_formatada = new Date(data_criacao)
 
-    return await prisma.pedido.create({
-        data: {
-            cliente,
-            data_criacao: data_criacao_formatada,
-            status,
-            valor_total,
-            pedidoItems,
-        }
-    })
+  return await prisma.pedido.create({
+    data: {
+      cliente,
+      data_criacao: data_criacao_formatada,
+      status,
+      valor_total,
+      pedidoItems,
+    },
+  })
 }
 
 export const retornaStatus = async (id: number) => {
-    try{
-        const pedido = await retornaPedido(id)
-        const status = pedido.status
+  try {
+    const pedido = await retornaPedido(id)
+    const status = pedido.status
 
-        return status
-    }
-    catch(error){
-        return error
-    }
+    return status
+  } catch (error) {
+    return error
+  }
 }
