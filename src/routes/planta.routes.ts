90c572c952546b5fb305e309aa861498ac16b168
CarlosDinizz <carlosdzalmeida@gmail.com>
2024-11-05 18:31:16 -0300

    Documentação swagger para a rotina de disponibilidade de plantas

--- src/routes/planta.routes.ts
+++ src/routes/planta.routes.ts
@@ -7,6 +7,69 @@ const plantaRouter = Router()
 
 plantaRouter.post('/planta', verificarToken, cadastroPlanta)
 
+/**
+ * @swagger
+ * tags:
+ *   name: Plantas
+ *   description: Operações relacionadas às plantas
+ */
+/**
+/**
+* @swagger
+* 
+* /plantas/planta/{id}/{quantidade}:
+*   get: # Método GET para ver fornecedores e plantas disponíveis
+*       summary: Obter plantas e fornecedores disponíveis
+*       tags: [Plantas]
+*       parameters:
+*           - in: path
+*             name: id
+*             required: true
+*             description: Id da planta popular
+*             schema:
+*               type: integer
+*               example: 1
+*           - in: path
+*             name: quantidade
+*             required: true
+*             description: Quantidade de plantas a ser verificada
+*             schema:
+*               type: integer
+*               example: 1
+*       responses:
+*           201:
+*               description: Lista de plantas e fornecedores disponíveis retornados com sucesso
+*               content:
+*                   application/json:
+*                       schema:
+*                           type: array
+*                           items:
+*                                type: object
+*                                properties:
+*                                     success:
+*                                        type: boolean
+*                                        example: true
+*                                     fornecedor:
+*                                        type: integer
+*                                        example: 1
+*                                     quantidade:
+*                                        type: integer
+*                                        example: 1
+*           500:
+*               description: Erro interno no servidor
+*               content: 
+*                   application/json:
+*                       schema:
+*                           type: object
+*                           properties:
+*                               success:
+*                                   type: boolean
+*                                   example: false
+*                               error:
+*                                   type: string
+*                                   example: "Ocorreu um erro ao buscar as plantas"
+* 
+*/
 plantaRouter.get('/planta/:id/:quantidade', getFornecedorPlantasDisponiveis)
 
 export default plantaRouter
